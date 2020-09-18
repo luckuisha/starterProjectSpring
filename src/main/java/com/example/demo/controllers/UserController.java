2391ad1b38c3dc7e878306135245c3aea5ff0ed2
lucky <lucky.kim@caseware.com>
2020-09-18 11:10:21 -0400

    change requestParam to requestBody

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 import java.util.regex.Pattern;
@@ -27,32 +24,32 @@ public class UserController {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @PostMapping("/register")
-    public ResponseEntity register(@RequestParam("username") String name, @RequestParam("pw") String pw) {
+    public ResponseEntity register(@RequestBody String username, @RequestBody String pw) {
 
         if (!Pattern.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{10,18}$", pw)) {
             System.out.println("pw is bad");
             return new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
 
-        if (name.length() < 8 || name.length() > 30 || name.isBlank() || !name.matches("\\S+")){
+        if (username.length() < 8 || username.length() > 30 || username.isBlank() || !username.matches("\\S+")){
             System.out.println("username is bad");
             return new ResponseEntity(HttpStatus.BAD_REQUEST);
         }
 
-        if (userRepImpl.findByUsername(name).isPresent()) {
+        if (userRepImpl.findByUsername(username).isPresent()) {
             System.out.println("already exists");
             return new ResponseEntity(HttpStatus.CONFLICT);
         }
 
-        user = new User(name, pw);
+        user = new User(username, pw);
         userRepImpl.save(user);
         System.out.println("success");
         return new ResponseEntity(HttpStatus.OK);
     }
 
     @GetMapping("/login")
-    public ResponseEntity login(@RequestParam("username") String name, @RequestParam("pw") String pw) {
-        Optional<User> currentUser = userRepImpl.findByUsername(name);
+    public ResponseEntity login(@RequestBody String username, @RequestBody String pw) {
+        Optional<User> currentUser = userRepImpl.findByUsername(username);
 
         if (currentUser.isPresent()) {
             user = currentUser.get();
